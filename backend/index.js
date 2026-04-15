5a7e5d5cc1f6a0df821abd797b63f62d72ecbd6f
Yadeni <yadenil@users.noreply.github.com>
2026-04-15 08:08:01 +0300

    cors configured between vercel and render

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,26 @@ const cors = require('cors');
 const tasksRouter = require("./routes/tasks");
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://task-tracker-gamma-topaz.vercel.app/", 
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  }),
+);
 app.use(express.json());
 
 // Middleware to log all requests
